9179c34d670a3cab6d9740a53937c27fcec67d1f
guebszin <157021993+guebszin@users.noreply.github.com>
2025-04-10 19:41:48 -0300

    Update imoveis.js

--- imoveis.js
+++ imoveis.js
@@ -1,9 +1,9 @@
 const imoveis = [
     {
       id: 1,
-      titulo: 'Casa no Campo',
-      cidade: 'Atibaia',
-      preco: 350000,
+      titulo: 'Casa Moderna',
+      cidade: 'Duas Barras',
+      preco: 3500000,
       imagem: 'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       descricao: 'Casa espaçosa no interior com quintal grande e área verde. Ideal para famílias e quem busca tranquilidade.'
     },
